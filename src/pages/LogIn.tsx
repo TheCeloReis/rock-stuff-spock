07a85b352f43373a8cfadf45964fe61b30ac5010
Marcelo Reis <oi@marcelreis.dev>
2021-03-15 00:09:48 -0300

    feat: basic login page

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -7,12 +7,19 @@ function LogInPage() {
   const dispatch = useDispatch();
 
   return (
-    <div>
-      <h1>Login</h1>
-
-      <button onClick={() => dispatch(signInWithGoogle())}>
-        Login with Google
-      </button>
+    <div className="flex h-full items-center justify-center">
+      <div className="max-w-screen-sm w-full m-4 bg-white shadow-md rounded-md px-8 pt-6 pb-8 flex flex-col">
+        <h1 className="text-center text-3xl pb-8">Rock Stuff Spock</h1>
+        <div className="flex items-center justify-center">
+          <button
+            className="bg-blue-500 hover:bg-blue-900 text-white font-bold py-3 px-4 rounded"
+            type="button"
+            onClick={() => dispatch(signInWithGoogle())}
+          >
+            Sign in with Google
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
